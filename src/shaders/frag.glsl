0e1fbca22707a72e214751dfd8c01e287dc5c1b5
Harvey Hunt <harveyhuntnexus@gmail.com>
2016-06-18 20:56:51 +0100

    Add some interpolated colour

--- src/shaders/frag.glsl
+++ src/shaders/frag.glsl
@@ -1,7 +1,9 @@
 #version 140
 
+in vec2 col;
+
 out vec4 color;
 
 void main() {
-    color = vec4(0.0, 0.0, 1.0, 1.0);
+    color = vec4(col, 1.0, 1.0);
 }
--- src/shaders/vert.glsl
+++ src/shaders/vert.glsl
@@ -2,8 +2,11 @@
 
 in vec2 pos;
 
+out vec2 col;
+
 uniform mat4 matrix;
 
 void main() {
+    col = pos;
     gl_Position = matrix * vec4(pos, 0.0, 1.0);
 }
